9859c25f988a43bf49f74e05770cbbc7d053da08
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-24 11:29:26 -0500

    Creando Páginas de forma programática en función de los datos

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,42 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const productTemplate = path.resolve(`src/templates/Product.js`)
+  const result = await graphql(`
+    query GET_SKUS {
+      allStripeSku {
+        edges {
+          node {
+            id
+            price
+            product {
+              name
+              metadata {
+                description
+                img
+                wear
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  //Manejar los errores
+  if (result.errors) throw result.errors
+
+  // Si todo esta bien
+  result.data.allStripeSku.edges.forEach(({ node }) => {
+    // Creamos la pagina
+    createPage({
+      path: `${node.id}`,
+      component: productTemplate,
+    })
+  })
+}
