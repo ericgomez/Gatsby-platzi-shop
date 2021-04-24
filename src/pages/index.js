244f90cb5a953365708a58e21a39ba4d1af1df91
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-04-24 11:17:19 -0500

    Mostrando los Productos de Stripe en React

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-import { Jumbo } from "../components"
-
-import { SEO } from "../components"
-import styled from "styled-components"
+import { graphql } from "gatsby"
+import { Jumbo, SEO, Products } from "../components"
 
 // Consumo de Query en GraphQL
 export const query = graphql`
-  query GET_DESCRIPTION {
+  query GET_DATA {
     allSite {
       edges {
         node {
@@ -17,28 +14,36 @@ export const query = graphql`
         }
       }
     }
+    allStripeSku {
+      edges {
+        node {
+          id
+          price
+          product {
+            name
+            metadata {
+              description
+              wear
+              img
+            }
+          }
+        }
+      }
+    }
   }
 `
 
-const Button = styled.button`
-  width: 8rem;
-  background-color: #98ca3f;
-  border: none;
-  border-radius: 10px;
-  color: ${props => props.color};
-  &:hover {
-    transform: scale(1.2);
-  }
-`
-
-const IndexPage = ({ data }) => (
-  <>
-    <SEO title='Home' />
-    <Jumbo description={data.allSite.edges[0].node.siteMetadata.description} />
-    <Button color='gray'>Comprar</Button>
-    <Link to='/page-2/'>Go to page 2</Link>
-    <Link to='/gracias/'>Go to gracias</Link>
-  </>
-)
+const IndexPage = ({ data }) => {
+  console.log(data)
+  return (
+    <>
+      <SEO title='Home' />
+      <Jumbo
+        description={data.allSite.edges[0].node.siteMetadata.description}
+      />
+      <Products products={data.allStripeSku.edges} />
+    </>
+  )
+}
 
 export default IndexPage
--- src/utils/priceFormat.js
+++ src/utils/priceFormat.js
@@ -1,4 +1,4 @@
-export default function priceFormat(price) {
+export default function priceFormat (price) {
   const priceFloat = (price / 100).toFixed(2)
   return Intl.NumberFormat("en-US", {
     style: "currency",
